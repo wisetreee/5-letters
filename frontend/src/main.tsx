927d46b7fb3b89129d2b299188b0396e937760c3
Arseny <arsenid2003@gmail.com>
2025-03-19 20:11:44 +0500

    save changes for explaining

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,23 @@ import "./index.css";
 import App from "@/App.tsx";
 import { BrowserRouter } from "react-router";
 import Header from "./components/Header";
+import { init, miniApp } from '@telegram-apps/sdk-react';
+
+const initializeTelegramSDK = async () => {
+  try {
+    await init();
+
+    if (miniApp.ready.isAvailable()) {
+      await miniApp.ready();
+      console.log('Mini App готово');
+    }
+
+  } catch (error) {
+    console.error('Ошибка инициализации:', error);
+  }
+};
+
+initializeTelegramSDK();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
