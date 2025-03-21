acb828f6fd667f03e16f35ac6b91c597b133621c
Arseny <arsenid2003@gmail.com>
2025-03-21 17:50:37 +0500

    sync with develop and delete telegram sdk lib

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,29 +4,7 @@ import "./index.css";
 import App from "@/App.tsx";
 import { BrowserRouter } from "react-router";
 import Header from "./components/Header";
-import { init, miniApp } from '@telegram-apps/sdk-react';
-import { retrieveLaunchParams } from '@telegram-apps/sdk';
-import { InitData} from "@telegram-apps/sdk-react";
 
-
-const initializeTelegramSDK = async () => {
-  try {
-    await init();
-
-    if (miniApp.ready.isAvailable()) {
-      await miniApp.ready();
-      const { initDataRaw, initData } = retrieveLaunchParams();
-      console.log();
-      console.log('Mini App готово');
-      console.log(initData);
-    }
-
-  } catch (error) {
-    console.error('Ошибка инициализации:', error);
-  }
-};
-
-initializeTelegramSDK();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
