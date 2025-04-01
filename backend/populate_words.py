8d32a2b69ee310016e78c18fd52f88029af93c60
Oleg <54029296+InferKing@users.noreply.github.com>
2025-04-01 20:59:09 +0500

    fix some issues with populate_words.py

--- backend/populate_words.py
+++ backend/populate_words.py
@@ -1,26 +1,25 @@
-import sys
 import logging
-from flask import Flask
-from config import Config
-from database import db, init_db
+from database import db
 from models import Word, User
-import os
-
-app = Flask(__name__)
-app.config.from_object(Config)
-init_db(app)
 
 logging.basicConfig(level=logging.INFO)
 
 def load_words(file_path):
-    with open(file_path, 'r', encoding='cp1251') as file:
-        words = [line.strip() for line in file.readlines() if 3 <= len(line.strip()) <= 10 and '-' not in line]
-    logging.info(f"Loaded {len(words)} valid words from {file_path}.")
-    return words
+    try:
+        with open(file_path, 'r', encoding='cp1251') as file:
+            words = [line.strip() for line in file.readlines() if 3 <= len(line.strip()) <= 10 and '-' not in line]
+        logging.info(f"Loaded {len(words)} valid words from {file_path}.")
+        return words
+    except FileNotFoundError:
+        logging.error(f"File {file_path} not found!")
+        return []
+    except UnicodeDecodeError:
+        logging.error(f"Failed to decode {file_path} with cp1251 encoding!")
+        return []
 
-def populate_database(file_path):
+def populate_database(file_path, app):
     with app.app_context():
-        admin = User.query.filter_by(username="admin").first()
+        admin = User.query.filter_by(role="ADMIN").first()
         if not admin:
             admin = User(username="admin", role="ADMIN")
             db.session.add(admin)
@@ -31,8 +30,12 @@ def populate_database(file_path):
 
         words = load_words(file_path)
         added_count = 0
+
+        db_words = Word.query.all()
+        s_words = [word.word for word in db_words]
+
         for word in words:
-            if not Word.query.filter_by(word=word).first():
+            if word not in s_words:
                 new_word = Word(word=word, length=len(word), added_by=admin.id)
                 db.session.add(new_word)
                 added_count += 1
@@ -42,16 +45,3 @@ def populate_database(file_path):
             logging.info(f"Successfully added {added_count} words to the database.")
         else:
             logging.info("No new words to add.")
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python populate_words.py <path_to_word_file>")
-        sys.exit(1)
-    
-    file_path = sys.argv[1]
-
-    if not os.path.exists(file_path):
-        print(f"Error: File '{file_path}' not found.")
-        sys.exit(1)
-
-    populate_database(file_path)
