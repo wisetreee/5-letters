86196a7b2f0e4b30dc4876576bf24b5636b16c5c
Arseny <arsenid2003@gmail.com>
2025-04-09 18:36:26 +0500

    make sidebar for mobiles and add avatar instead of i

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import { ThemeProvider } from "@/components/theme/theme-provider";
 import EndlessModePage from "@/pages/EndlessModePage";
 import { useInitApp } from "@/hooks/useInitApp";
 import { useAuth } from "./hooks/useAuth";
+import { SidebarProvider } from "./components/ui/sidebar";
+import Header from "./components/Header";
 
 
 const App = () => {
@@ -12,17 +14,22 @@ const App = () => {
 
     return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      
       {loading ? 
       <div>Загрузка...</div> : 
       !isAuth ? 
       <div>Ошибка авторизации: {authError} </div> :
+      <SidebarProvider>
+        <Header />
       <main>
         <Routes>
           <Route index element={<HomePage />} />
           <Route path="/game/endless" element={<EndlessModePage />} />
         </Routes>
       </main>
+      </SidebarProvider>
       }
+      
     </ThemeProvider> 
   )
 };
---
+++ frontend/src/components/ui/SidebarTriggerButton.tsx
@@ -0,0 +1,13 @@
+import { useSidebar } from "@/components/ui/sidebar"
+import { Button } from "@/components/ui/button"
+import { AlignJustify } from "lucide-react"
+ 
+export const SidebarTriggerButton = () => {
+  const { toggleSidebar } = useSidebar()
+ 
+  return (
+  <Button className="sm:hidden" size="icon" variant="ghost" onClick={toggleSidebar}>
+    <AlignJustify />
+  </Button>
+    )
+}
